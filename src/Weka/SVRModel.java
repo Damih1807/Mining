d414fff1f82e776a8c26de85f5db319849758137
Damih1807 <126248430+Damih1807@users.noreply.github.com>
2024-05-19 22:20:36 +0700

    Update SVRModel.java

--- src/Weka/SVRModel.java
+++ src/Weka/SVRModel.java
@@ -1,28 +1,32 @@
 package Weka;
-
-import weka.classifiers.Evaluation;
-import weka.classifiers.functions.SMOreg;
 import weka.core.Instances;
-import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import weka.classifiers.functions.SMOreg;
+import weka.classifiers.Evaluation;
 import java.io.File;
-
 public class SVRModel {
     private Instances trainData;
     private Instances evalData;
     private SMOreg model;
+    private Evaluation eval;
 
-    public SVRModel(String arffFilePath) throws Exception {
-        File arffFile = new File(arffFilePath);
-        
-        // Kiểm tra sự tồn tại của file ARFF
-        if (!arffFile.exists()) {
-            throw new Exception("File ARFF không tồn tại.");
-        }
-
-        // Load dữ liệu từ file ARFF
-        ArffLoader loader = new ArffLoader();
-        loader.setFile(arffFile);
+    public SVRModel(String csvFilePath) throws Exception {
+        // Load dữ liệu từ file CSV
+        csvFilePath = "C:/My Downloads/Preprocessing/Data/cleanedData.csv";
+        CSVLoader loader = new CSVLoader();
+        loader.setSource(new File(csvFilePath));
         Instances data = loader.getDataSet();
+
+        // Chuyển đổi dữ liệu từ CSV sang ARFF
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(data);
+        File arffFile = new File("CleanedData.arff");
+        saver.setFile(arffFile);
+        saver.setDestination(arffFile);
+        saver.writeBatch();
+
+        System.out.println("Chuyển đổi thành công từ CSV sang ARFF.");
 
         // Chia tập dữ liệu thành tập huấn luyện và tập đánh giá
         data.randomize(new java.util.Random(1));
@@ -35,22 +39,40 @@ public class SVRModel {
         trainData.setClassIndex(trainData.attribute("WIND").index());
         evalData.setClassIndex(evalData.attribute("WIND").index());
 
-        // Khởi tạo mô hình SVR
+        // Khởi tạo mô hình hồi quy tuyến tính
         model = new SMOreg();
 
-        // Huấn luyện mô hình trên tập huấn luyện
+        // Huấn luyện mô hình trên tập huấn luyện và đo thời gian
+        long startTime = System.nanoTime();
         model.buildClassifier(trainData);
+        long buildTime = System.nanoTime() - startTime;
 
-        // Đánh giá mô hình bằng phương pháp cross-validation
-        Evaluation eval = new Evaluation(trainData);
-        eval.evaluateModel(model, evalData);
+        // Đánh giá mô hình bằng phương pháp cross-validation 10-fold và đo thời gian
+        startTime = System.nanoTime();
+        eval = new Evaluation(trainData);
+        eval.crossValidateModel(model, trainData, 10, new java.util.Random(1));
+        long evalTime = System.nanoTime() - startTime;
 
         // In kết quả đánh giá
         System.out.println("Evaluation Results:");
         System.out.println(eval.toSummaryString());
+        System.out.println("Correlation coefficient: " + eval.correlationCoefficient());
+        System.out.println("Mean absolute error: " + eval.meanAbsoluteError());
+        System.out.println("Root mean squared error: " + eval.rootMeanSquaredError());
+        System.out.println("Relative absolute error: " + eval.relativeAbsoluteError() + " %");
+        System.out.println("Root relative squared error: " + eval.rootRelativeSquaredError() + " %");
+        System.out.println("Total Number of Instances: " + eval.numInstances());
+        System.out.println("Build Time (seconds): " + buildTime / 1e9);
+        System.out.println("Evaluation Time (seconds): " + evalTime / 1e9);
 
         // In độ chính xác
         printRSquared();
+
+        // Dự đoán giá trị và đo thời gian
+        startTime = System.nanoTime();
+        double[] predictions = predict();
+        long predictTime = System.nanoTime() - startTime;
+        System.out.println("Prediction Time (seconds): " + predictTime / 1e9);
     }
 
     public double[] predict() throws Exception {
@@ -61,46 +83,8 @@ public class SVRModel {
         return predictions;
     }
 
-    public double calculateCorrelation() throws Exception {
-        double[] predictions = predict(); // Dự đoán giá trị cho dữ liệu đánh giá
-        double[] actual = new double[evalData.numInstances()];
-        for (int i = 0; i < evalData.numInstances(); i++) {
-            actual[i] = evalData.instance(i).classValue(); // Lấy giá trị thực tế của biến mục tiêu
-        }
-        return calculateCorrelation(predictions, actual);
-    }
-
-    private double calculateCorrelation(double[] predicted, double[] actual) {
-        // Tính toán giá trị tương quan giữa dự đoán và giá trị thực tế
-        double sumXY = 0;
-        double sumX2 = 0;
-        double sumY2 = 0;
-        double sumX = 0;
-        double sumY = 0;
-        int n = predicted.length;
-        for (int i = 0; i < n; i++) {
-            sumXY += predicted[i] * actual[i];
-            sumX += predicted[i];
-            sumY += actual[i];
-            sumX2 += Math.pow(predicted[i], 2);
-            sumY2 += Math.pow(actual[i], 2);
-        }
-        double correlation = (n * sumXY - sumX * sumY) / Math.sqrt((n * sumX2 - Math.pow(sumX, 2)) * (n * sumY2 - Math.pow(sumY, 2)));
-        return correlation;
-    }
-
-    public void printCorrelation() throws Exception {
-        double correlation = calculateCorrelation();
-        System.out.println("Correlation coefficient: " + correlation);
-    }
-
-    public double calculateRSquared() throws Exception {
-        double correlation = calculateCorrelation(); // Tính toán giá trị tương quan
-        return Math.pow(correlation, 2); // Tính toán R-squared bằng cách bình phương giá trị tương quan
-    }
-
     public void printRSquared() throws Exception {
-        double rSquared = calculateRSquared();
+        double rSquared = eval.correlationCoefficient() * eval.correlationCoefficient();
         System.out.println("R-squared: " + rSquared);
     }
 }
